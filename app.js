cec0b0bff07149b85a85d3b1bd9b0eff27441162
Zacarías <santiago1988quispep@gmail.com>
2021-06-26 16:14:08 -0300

    mercado liebre responsive

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require ('express');
+const app = express();
+const path = require ('path');
+const port = 5000;
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'index.html')));
+
+app.listen(port , ()=>console.log(`Corriendo en Servidor ${port}`));
