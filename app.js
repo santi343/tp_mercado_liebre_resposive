967c0b96111313e27f8fb766ff40a3d8ec212d75
Zacarías <santiago1988quispep@gmail.com>
2021-06-29 21:02:10 -0300

    mejorando resposive

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const app = express();
 const path = require ('path');
-const port = 5000;
+const port = 3030;
 app.use(express.static('public'));
 
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'index.html')));
